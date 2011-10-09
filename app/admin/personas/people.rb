54792cbd7f47b7dfac365c61cea11723abfae319
Olvap <oldani.pablo@gmail.com>
2011-10-09 13:16:38 -0300

    fix: people

--- app/admin/personas/people.rb
+++ app/admin/personas/people.rb
@@ -1,11 +1,24 @@
 ActiveAdmin.register Person do
 
+  menu :label => "Personas"
+
+  scope :all, :default => true
   scope :profesionales
+  scope :empleados
 
   controller do
     load_and_authorize_resource
     skip_load_resource :only => :index
-  end 
+  end
+
+  show do
+	
+    section do
+      attributes_table_for person,
+       :name, :born, :tdoc, :doc, :civil, :sexo, :observations
+    end
+
+  end
 
   form do |f|
 
@@ -38,7 +51,21 @@ ActiveAdmin.register Person do
           link_to "editar", edit_admin_person_address_path(person,a)
         end
       end
-    link_to "Ver todas", admin_person_addresses_path(person)
+    link_to "Administrar", admin_person_addresses_path(person)
+  end
+
+  sidebar "Profesion", :only => :show do
+    if person.profesionals.count > 0
+      table_for person.profesionals do
+        column :titulo
+        column :matricula
+        column "Acciones" do |a|
+          link_to "editar", edit_admin_person_profesional_path(person,a)
+        end
+
+      end
+      link_to "Administrar", admin_person_profesionals_path(person)
+    end
   end
 
 #  sidebar :help
@@ -47,6 +74,14 @@ ActiveAdmin.register Person do
 
   action_item(:only => :show) do
     link_to("agregar direccion de contacto", new_admin_person_address_path(person)) +
-    link_to("agregar Profesion", new_admin_person_profesional_path(person))
-  end 
+    link_to("agregar Profesion", new_admin_person_profesional_path(person)) +
+    (link_to("Convertir en empleado", new_admin_person_empleado_path(person)) if !person.empleado?) +
+    (link_to("Ver datos de empleado", admin_person_empleado_path(person,person.empleado)) if person.empleado?)
+  end
+
+  member_action :empleados, :method => :put do
+    person = Person.find(params[:id])
+    redirect_to :action => :show, :notice => "Locked!"
+  end
+
 end
