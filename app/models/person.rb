f070c14855f591aea4df2434470760ce323eb840
Olvap <oldani.pablo@gmail.com>
2011-10-20 00:24:34 -0300

    muevo al person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,2 @@
 class Person < Personas::Person
-  has_many :profesionals
-  has_one :empleado
-
-  scope :profesionales, joins(:profesionals) #& where(:id=>1)
-  scope :empleados, joins(:empleado) #& where(:id=>1)
-
-  def empleado?
-    empleado
-  end
 end
