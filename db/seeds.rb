4bf18b94c5f468681e917f021b0523ad08bfd74a
Olvap <oldani.pablo@gmail.com>
2011-11-01 11:32:39 -0300

    agrego seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+tdocs=["DNI","CUIT","CUIL","LE","LC","Pasaporte"]
+civils=["Soltero","Casado","Divorciado","Viudo"]
+sexos=["Masculino","Femenino"]
+taddresses=["Particular","Oficina"]
+categories = ['Catastro', 'Urbano']
+oficinas = ['Catastro','Planeamiento urbano','Profesional','Archivo']
+
+puts "** Oficinas **"
+oficinas.each do |tipo|
+  Expedientes::Oficina.find_or_create_by_name(tipo)
+  puts tipo
+end
+
+puts "** Tipo de documentos **"
+tdocs.each do |tipo|
+  Personas::Tdoc.find_or_create_by_name(tipo)
+  puts tipo
+end
+
+puts "** Tipo de sexo **"
+sexos.each do |tipo|
+  Personas::Sexo.find_or_create_by_name(tipo)
+  puts tipo
+end
+
+
+
+puts "** Tipo de estados civiles **"
+civils.each do |tipo|
+  Personas::Civil.find_or_create_by_name tipo
+  puts tipo
+end
+
+puts "** Tipo de contactos **"
+taddresses.each do |tipo|
+  Personas::Taddress.find_or_create_by_name tipo
+  puts tipo
+end
+
+puts "** Tipo de expedientes **"
+categories.each do |tipo|
+  Category.find_or_create_by_name tipo
+  puts tipo
+end
