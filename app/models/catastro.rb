75ecc8ab7af144f56cadb716698efbe275f2bc1b
Olvap <oldani.pablo@gmail.com>
2011-11-03 09:58:50 -0300

    ADD: links para versionado

--- app/models/catastro.rb
+++ app/models/catastro.rb
@@ -1,2 +1,9 @@
 class Catastro < Expedientes::Expediente
+
+  include Rails.application.routes.url_helpers # neeeded for _path helpers to work in models
+
+  def admin_permalink
+    admin_catastro_path(self)
+  end
+
 end
