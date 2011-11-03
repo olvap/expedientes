ff9ab9309de2542a80ab89b2a88dd891c7c7f4ec
Olvap <oldani.pablo@gmail.com>
2011-11-03 10:17:46 -0300

    FIX: arrelgo la vista de users

--- app/admin/users/admin_users.rb
+++ app/admin/users/admin_users.rb
@@ -1,6 +1,11 @@
 ActiveAdmin.register AdminUser do
   menu :parent => "Users"
 
+  controller do
+    load_and_authorize_resource
+    skip_load_resource :only => :index
+  end
+
   index do
     column :email
     column :current_sign_in_at
@@ -8,6 +13,22 @@ ActiveAdmin.register AdminUser do
     column :sign_in_count
     default_actions
   end
+
+  show do
+    panel "Usuario" do
+      attributes_table_for admin_user,
+        :id, :email, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :created_at, :updated_at
+     
+    end
+
+    panel "Roles" do
+      table_for admin_user.roles do
+        column :name
+        column :observations
+      end
+    end
+  end
+
 
   form do |f|
     f.inputs "Admin Details" do 
