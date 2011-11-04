3ee26768643e24af77fca0472dd82d2a691301e2
Olvap <oldani.pablo@gmail.com>
2011-11-04 09:52:52 -0300

    FIX: agrego datos de empleado y profesional en el modelo que está fuera del plugin

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,16 @@
 class Person < Personas::Person
+
+  def empleado?
+    empleado
+  end
+
+  def admin_permalink
+    admin_person_path(self)
+  end
+
+  scope :profesionales, joins(:profesionals) #& where(:id=>1)
+  scope :empleados, joins(:empleado) #& where(:id=>1)
+
+  has_many :profesionals
+  has_one :empleado
 end
