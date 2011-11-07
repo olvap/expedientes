07d21aebd11e15bfe834da6067bf307b2ac7a0ab
Olvap <oldani.pablo@gmail.com>
2011-11-07 12:18:50 -0300

    ADD: comments

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,7 +6,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Expedientes"
-  config.allow_comments_in = [] 
+  #config.allow_comments_in = ["Catstro"] 
   # Set the link url for the title. For example, to take 
   # users to your main site. Defaults to no link.
   #
