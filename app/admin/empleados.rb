4b4ed15aa3e2ec831ae636bc80675dc125e59a00
Olvap <oldani.pablo@gmail.com>
2011-11-07 12:22:38 -0300

    FIX: general

--- app/admin/empleados.rb
+++ app/admin/empleados.rb
@@ -5,10 +5,36 @@ ActiveAdmin.register Empleado do
     f.inputs "Details" do
       f.input :inicio, :as=>:string, :input_html => {:class => 'datepicker'}
       f.input :legajo
-      f.input :person
     end
 
     f.buttons
+  end
+
+  action_item(:only => :show) do
+    link_to("Cursos", admin_person_cursos_path(person)) +
+    link_to("Agregar Curso", new_admin_person_curso_path(person))
+  end
+  
+  show do
+    panel "Detalles" do
+      attributes_table_for empleado,
+         :legajo, :inicio, :created_at, :updated_at
+
+    end
+    panel "Cursos" do
+      table_for empleado.person.cursos do
+        column :name
+        column :inicio
+        column :duracion
+        column :institucion
+        column :titulo
+        column "Acciones" do |a|
+          link_to "editar", edit_admin_person_curso_path(person,a)
+        end
+      end
+      link_to "Administrar", admin_person_cursos_path(person)
+    end
+
   end
 
 end
