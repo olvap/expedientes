843e07a1dd5b4a8891a0fc784eb10c796cb65c86
Olvap <oldani.pablo@gmail.com>
2011-11-07 12:20:23 -0300

    saco el plugin de persona

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,18 @@
-class Address < Personas::Address
+class Address < ActiveRecord::Base
+  include Rails.application.routes.url_helpers # neeeded for _path helpers to work in models
+  has_paper_trail
+
+  def admin_permalink
+    admin_person_address_path(self.person,self)
+  end
+
+  # FIX: cuando se solucione el problema de rails.
+
+  belongs_to :taddress
+  belongs_to :person
+  #belongs_to :city
+
+  def format
+    "#{street} #{number} #{other}"
+  end
 end
