b80b7c0e2e41ea6509d22fb6e6ba900d560fd6c8
Olvap <oldani.pablo@gmail.com>
2011-11-19 01:24:09 -0300

    FIX: saco family de personas, agreo un modelo para family

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,6 +5,8 @@ class Person < ActiveRecord::Base
   #is_person
   has_paper_trail
 
+  has_one :family
+
   has_many :profesionals
   has_many :cursos
   has_many :empleados
@@ -14,10 +16,6 @@ class Person < ActiveRecord::Base
   belongs_to :tdoc
   has_many :matrimonios, :class_name => 'Matrimonio',:foreign_key => "person1_id"
   has_many :matrimonios_seccond, :class_name => 'Matrimonio',:foreign_key => "person2_id"
-  belongs_to :pather,:class_name => "Person", :foreign_key => "pather_id"
-  belongs_to :mother,:class_name => "Person", :foreign_key => "mother_id"
-  has_many :hijosv,:class_name => "Person", :foreign_key => "pather_id"
-  has_many :hijosm,:class_name => "Person", :foreign_key => "mother_id"
 
   scope :profesionales, joins(:profesionals) #& where(:id=>1)
   scope :empleados, joins(:empleados) #& where(:id=>1)
@@ -43,6 +41,12 @@ class Person < ActiveRecord::Base
 
   #end lock
 
+  #version
+  def version
+    versions.count
+  end
+  #end version
+
   def format
     name
   end
@@ -54,18 +58,10 @@ class Person < ActiveRecord::Base
     (empleados.count > 0)
   end
 
-  def familiaridad(p)
-    r = (estado_pareja p).try(:estado) || "Hermano"
-    r = "Padre" if (p == pather)
-    r = "Madre" if (p == mother)
-    r = "hijo" if (p.pather == self)
-    r = "hijo" if (p.mother == self)
-    r
-  end
-
   def parejas?
     ( parejas.count > 0 )
   end
+
   def estado_pareja p
     if parejas? and parejas.index p
       pareja = parejas[parejas.index p]
@@ -89,57 +85,9 @@ class Person < ActiveRecord::Base
     end
   end
 
-  def hijos
-    h = []
-    h = h + (hijosv if hijosv)+ (hijosm if hijosm)
-  end
-
-  def hermanos
-    h = []
-    
-    h = pather.try(:hijosv) + pather.try(:hijosm) if pather
-    h = mother.try(:hijosv) + mother.try(:hijosm) if mother
-    h
-  end
-
-  def padres
-    p = []
-    p = p + [pather] + [mother]
-
-  end
-  
-  def familiares
-    f = []
-    f = f + padres + hijos + hermanos + parejas
-    check_uniq f
-  end 
-
   def parejas
     p = []
     p = p + matrimonios.map{|k,v|k.person2} + matrimonios_seccond.map{|k,v|k.person1}
   end
 
-  def fathers
-    if pather
-      [pather]
-    else
-      []
-    end
-  end
-  
-  def mothers
-    if mother
-      [mother]
-    else
-      []
-    end
-  end
-
-  private
-  def check_uniq(object)
-    object.delete nil
-    object.delete self
-    object.uniq!
-    object
-  end
 end
