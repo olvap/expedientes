7dc53db8afb4aba566c0350016443edfb27561cd
sevidor-rails <rails@rails-G31M-ES2C.(none)>
2011-12-01 11:55:43 -0300

    FIX: saco comments del schema

--- db/schema.rb
+++ db/schema.rb
@@ -111,20 +111,6 @@ ActiveRecord::Schema.define(:version => 20111125190831) do
     t.datetime "updated_at"
   end
 
-  create_table "comments", :force => true do |t|
-    t.string   "title",            :limit => 50, :default => ""
-    t.text     "comment"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
-  add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
-  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
-
   create_table "cursos", :force => true do |t|
     t.integer  "person_id"
     t.string   "name"
