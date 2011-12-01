49fec3255249dbaed75cb81bd93b1a6874b168e5
sevidor-rails <rails@rails-G31M-ES2C.(none)>
2011-12-01 12:17:51 -0300

    FIX: cambio el tipo de name a un string

--- db/migrate/20111121190035_create_periodos.rb
+++ db/migrate/20111121190035_create_periodos.rb
@@ -1,7 +1,7 @@
 class CreatePeriodos < ActiveRecord::Migration
   def change
     create_table :periodos do |t|
-      t.integer :name
+      t.string :name
       t.date :vencimiento
 
       t.timestamps
