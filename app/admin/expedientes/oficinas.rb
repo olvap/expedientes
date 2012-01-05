29826b5f1b28278baf540f21a089fcc700e148eb
rails <rails@rails-desktop.(none)>
2012-01-05 14:54:27 -0300

    PIMP: mejoro la vista de pases

--- app/admin/expedientes/oficinas.rb
+++ app/admin/expedientes/oficinas.rb
@@ -5,8 +5,8 @@ ActiveAdmin.register Oficina,:as => "oficina" do
       table_for(oficina.pases) do |t|
         t.column(:catastro)
         t.column(:estado) {|order| status_tag(order.estado)  }
-        t.column(:created_at)
-        t.column(:updated_at)
+        t.column(:entrada)
+        t.column(:observaciones)
       end
     end
 
