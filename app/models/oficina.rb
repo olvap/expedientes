30fe35e38b363d013ccbd68eb68d8db7a1f7f019
Olvap <oldani.pablo@gmail.com>
2012-01-05 17:59:04 -0300

    ADD: agrego relacion para ver pases desde oficina

--- app/models/oficina.rb
+++ app/models/oficina.rb
@@ -2,6 +2,7 @@ class Oficina < ActiveRecord::Base
 
   has_paper_trail
   has_many :pases
+  has_many :catastros, :through => :pases
 
   include Rails.application.routes.url_helpers # neeeded for _path helpers to work in models
 
