482d8c417f00e3bc0b564bcd7f97fb50d2963dbe
Olvap <oldani.pablo@gmail.com>
2012-01-05 17:50:04 -0300

    FIX: en oficina solo muestro los expedientes pendientes en esa oficina.

--- app/admin/expedientes/oficinas.rb
+++ app/admin/expedientes/oficinas.rb
@@ -1,10 +1,21 @@
 ActiveAdmin.register Oficina,:as => "oficina" do
   menu :parent => "Comunes"
+
+  controller do
+
+    def show
+      @pases = []
+      @oficina = Oficina.find(params[:id])
+      @oficina.catastros.each{|c| @pases = @pases + [c.pases.last] if c.pases.last.oficina == @oficina }
+      show!
+    end
+
+  end
   show :title => :name do
     panel oficina.name do
-      table_for(oficina.pases) do |t|
-        t.column(:catastro)
-        t.column(:estado) {|order| status_tag(order.estado)  }
+      table_for(pases) do |t|
+        t.column(:catastro_id)
+        t.column(:estado) {|order| status_tag(order.try(:estado))  }
         t.column(:entrada)
         t.column(:observaciones)
       end
