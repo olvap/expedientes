ba617deea5992071c658729edb9a56ecc11e163a
Olvap <oldani.pablo@gmail.com>
2012-01-25 10:14:35 -0300

    PIMP: Admin people

--- app/admin/personas/people.rb
+++ app/admin/personas/people.rb
@@ -21,23 +21,23 @@ ActiveAdmin.register Person do
     default_actions
   end
   show :title => :name do
-    div(:id => "notice")do
-      params[:notice]
-    end
     div(:id => "xtabs") do
       ul do
         li link_to "Detalles", "#xtabs-1"
         li link_to "Expedientes", "#xtabs-8"
-        if can? :magane, Person
-          li link_to "Comercial", "#xtabs-7"
-          li link_to "Contacto", "#xtabs-2"
-          li link_to "Familiares", "#xtabs-3"
+        if can? :manage, Catastro
           li link_to "Profesion", "#xtabs-4"
-          li link_to "Empleado", "#xtabs-5"
-          li link_to "Formacion", "#xtabs-6"
         end
+        if can? :create, Address
+          li link_to "Contacto", "#xtabs-2"
+        end
+#        if can? :create, VER
+#          li link_to "Comercial", "#xtabs-7"
+#          li link_to "Familiares", "#xtabs-3"
+#          li link_to "Empleado", "#xtabs-5"
+#          li link_to "Formacion", "#xtabs-6"
+#        end
       end
-
       div(:id=> "xtabs-8") do
         panel "" do
           table_for person.catastros do
@@ -46,98 +46,99 @@ ActiveAdmin.register Person do
           end
         end
       end
-
       div(:id=> "xtabs-1") do
           attributes_table_for person,
           :name, :born, :tdoc, :doc, :civil, :sexo, :dead
       end
-
-      div(:id=> "xtabs-2") do
-        panel "Ubicaciones" do
-          table_for person.addresses do
-            column :format, :as=>"Direccion"
-            column :telefono
-            column :email
-            column "Acciones" do |a|
-              link_to("Detalle ", admin_person_address_path(person,a)) +
-              link_to(" Editar", edit_admin_person_address_path(person,a))
-            end
-
-          end
-          div link_to "Nueva ubicacion de contacto", new_admin_person_address_path(person)
-        end
-      end
-
-      if can? :manage, Person
-        div(:id => "xtabs-7") do
-          if person.comercy
-            attributes_table_for person.comercy,
-              :ingresos_brutos, :cuit, :cantidad_de_personal, :jubilado
-          end
-          link_to "Administrar", new_admin_person_comercy_path(person)
-        end
-
-        div(:id => "xtabs-3") do
-          panel "Matrimonios" do
-            table_for person.parejas do
-              column :name do |p| link_to p.name, admin_person_path(p) end
-            end
-            link_to "Administrar", admin_person_matrimonios_path(person)
-          end
-          panel "Familiares" do
-            if person.family
-              table_for person.family.parents do
-                 column do |p| link_to p.name, admin_person_path(p) end
-                 column do"padre/madre"end
-              end
-            end
-
-            table_for Family.hermanos(person)do
-              column do |a| link_to a.person.name, admin_person_path(a.person) end
-              column do "hermano/a"end
-            end
-
-            table_for Family.hijos_de person.id do
-              column do |a| link_to a.person.name, admin_person_path(a.person) end
-              column do"hijo"end
-            end
-
-            link_to "Administrar", new_admin_person_family_path(person)
-
-          end
-        end
-        div(:id => "xtabs-4") do
-          panel "Profesion" do
-            table_for person.profesionals do
-              column :titulo
-              column :matricula
+      if can? :create, Address
+        div(:id=> "xtabs-2") do
+          panel "Ubicaciones" do
+            table_for person.addresses do
+              column :format, :as=>"Direccion"
+              column :telefono
+              column :email
               column "Acciones" do |a|
-                link_to "editar", edit_admin_person_profesional_path(person,a)
+                link_to("Detalle ", admin_person_address_path(person,a)) +
+                link_to(" Editar", edit_admin_person_address_path(person,a))
               end
             end
-          link_to "Administrar", admin_person_profesionals_path(person)
+            div link_to "Nueva ubicacion de contacto", new_admin_person_address_path(person)
           end
-        end
-
-        div(:id => "xtabs-5") do
-          panel "Empleado" do
-            table_for person.empleados, do
-              column :legajo
-              column :inicio
-            end
-            link_to "Administrar", admin_person_empleados_path(person)
-          end
-        end
-
-        div(:id => "xtabs-6") do
-          panel "Cursos" do
-            table_for person.cursos
-            link_to "Administrar", admin_person_cursos_path(person)
-          end
-
         end
       end
+##      if can? :manage, VER
+##        div(:id => "xtabs-7") do
+##          if person.comercy
+##            attributes_table_for person.comercy,
+##              :ingresos_brutos, :cuit, :cantidad_de_personal, :jubilado
+##          end
+##          link_to "Administrar", new_admin_person_comercy_path(person)
+##        end
 
+##        div(:id => "xtabs-3") do
+##          panel "Matrimonios" do
+##            table_for person.parejas do
+##              column :name do |p| link_to p.name, admin_person_path(p) end
+##            end
+##            link_to "Administrar", admin_person_matrimonios_path(person)
+##          end
+##          panel "Familiares" do
+##            if person.family
+##              table_for person.family.parents do
+##                 column do |p| link_to p.name, admin_person_path(p) end
+##                 column do"padre/madre"end
+##              end
+##            end
+
+##            table_for Family.hermanos(person)do
+##              column do |a| link_to a.person.name, admin_person_path(a.person) end
+##              column do "hermano/a"end
+##            end
+
+##            table_for Family.hijos_de person.id do
+##              column do |a| link_to a.person.name, admin_person_path(a.person) end
+##              column do"hijo"end
+##            end
+
+##            link_to "Administrar", new_admin_person_family_path(person)
+
+##          end
+##        end
+        if can? :manage, Catastro
+          div(:id => "xtabs-4") do
+            panel "Profesion" do
+              table_for person.profesionals do
+                column :titulo
+                column :matricula
+                column "Acciones" do |a|
+                  link_to "editar", edit_admin_person_profesional_path(person,a)
+                end
+              end
+            link_to "Administrar", admin_person_profesionals_path(person)
+            end
+          end
+        end
+
+#        if can? :create, Ver
+#        div(:id => "xtabs-5") do
+#          panel "Empleado" do
+#            table_for person.empleados, do
+#              column :legajo
+#              column :inicio
+#            end
+#            link_to "Administrar", admin_person_empleados_path(person)
+#          end
+#        end
+
+#        div(:id => "xtabs-6") do
+#          panel "Cursos" do
+#            table_for person.cursos
+#            link_to "Administrar", admin_person_cursos_path(person)
+#          end
+
+#        end
+#      end
+#       end
     end
 
     active_admin_comments #if !params[:version]
