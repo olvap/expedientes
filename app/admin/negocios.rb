c56ab141ce8d9df5bb1f2105c5cb044c39627f86
Olvap <oldani.pablo@gmail.com>
2012-02-23 17:20:53 -0300

    ADD: filter. agrego para buscar por responsable en negocios

--- app/admin/negocios.rb
+++ app/admin/negocios.rb
@@ -5,11 +5,14 @@ ActiveAdmin.register Negocio do
 
   filter :name
   filter :rubro
+  filter :bromatologia_person_name,:as => :string, :label => "Responsable"
+#  filter :rubro_sub_rubro,:as => :number, :label => "Categoria"
 
   index do
     column :bromatologia
     column :name
     column :rubro
     column :responsable do |negocio| negocio.try(:bromatologia).try(:person).try(:name) end
+    column :categoria do |negocio| negocio.try(:rubro).try(:sub_rubro)end
   end
 end
