5f2059eeca865ce11f62754e9e8716f44609f181
Olvap <oldani.pablo@gmail.com>
2012-03-11 20:08:27 -0300

    FIX: me falto una s en tgi. cambio tgi por tgis

--- db/migrate/20120306210613_add_plano_mesura_codigo_judicial_to_tgis.rb
+++ db/migrate/20120306210613_add_plano_mesura_codigo_judicial_to_tgis.rb
@@ -1,6 +1,6 @@
 class AddPlanoMesuraCodigoJudicialToTgis < ActiveRecord::Migration
   def change
-    add_column :tgis, :plano_mesura, :string
-    add_column :tgi, :codigo_judicial, :integer
+    #add_column :tgis, :plano_mesura, :string
+    add_column :tgis, :codigo_judicial, :integer
   end
 end
