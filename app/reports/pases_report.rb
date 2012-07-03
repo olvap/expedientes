e84e432d98fd6f9f056ce96d3004961678a6abe8
Pablo oldani <oldani.pablo@gmail.com>
2012-07-03 12:04:27 -0300

    REMOVE: Prawn

--- app/reports/pases_report.rb
+++ app/reports/pases_report.rb
@@ -1,17 +1,4 @@
-require "layouts/header.rb"
-require 'prawnbot'
-
-class PasesReport < Prawnbot::Report
-
-  def initialize
-    super
-    @header_title = "MUNICIPALIDAD DE LA CIUDAD DE CORONDA"
-    @header_subtitle = "Sistema de expedientes"
-
-    @logo = "#{Rails.root}/app/assets/images/logo.png"
-    @water_print = "#{Rails.root}/app/assets/images/water_mark.png"
-
-  end
+class PasesReport
 
   def detalle(pase)
 
@@ -31,27 +18,4 @@ class PasesReport < Prawnbot::Report
 
   end
 
-  def show(pase)
-    self.font_size = 10
-    body
-
-    show_title "DETALLE DE PASE"
-
-    show_title "#{pase.expediente.inicio.name} N: #{pase.expediente.id} "
-
-    myform([
-      "<b>Partida</b> #{pase.expediente.partida}",
-      "<b>Responsanble</b> #{pase.expediente.responsable}",
-      "<b>Numero de expediente</b> #{pase.expediente.numero_expediente_colegio}",
-      "<b>Convenio</b> #{pase.expediente.convenio_id}",
-
-      "<b>Importe</b> #{pase.expediente.importe}"])
-
-
-    show_title "<b>Oficina actual</b> #{pase.expediente.oficina.name}"
-
-    mybox(pase.observaciones)
-
-    render
-  end
 end
