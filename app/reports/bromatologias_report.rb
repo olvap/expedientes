a1700138a8a3014c2ad7277cd211f3c52c9f2937
Pablo oldani <oldani.pablo@gmail.com>
2012-07-27 14:35:56 -0300

    FIX: modificaciones en el formulario de bromatologia

--- app/reports/bromatologias_report.rb
+++ app/reports/bromatologias_report.rb
@@ -7,7 +7,7 @@ class BromatologiasReport
         s.add_field(:periodo)     { |item| item.periodo_name.to_s }
         s.add_field(:vencimiento) { |item| item.periodo_vencimiento.to_s }
         s.add_field(:responsable) { |item| item.tributable_person_name  .to_s }
-        s.add_field(:ubicacion)   { |item| item.tributable_address.to_s }
+        s.add_field(:ubicacion)   { |item| item.tributable_address.try(:format).to_s }
         s.add_field(:importe)     { |item| item.calcular_deuda.to_s }
         s.add_field(:nombre) { |item| item.tributable.negocios.first.name.to_s }
         s.add_field(:negocio) { |item| item.tributable.negocios.first.rubro.name[0..20].to_s }
